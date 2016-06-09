9374b5e59a7df3fb2998ea3b011c66454e02dd90
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-06-09 15:08:11 -0400

    Config change

--- Plock.rb
+++ Plock.rb
@@ -31,17 +31,25 @@ class Plock < ActiveRecord::Base
   #   end
   # end
 
-get "/users/my_bookmarks" do
-  bookmarks = Bookmark.where(user_id: user.id)
-  bookmarks.all.to_json
-end
+# get "/users/my_bookmarks" do
+#   bookmarks = Bookmark.where(user_id: user.id)
+#   bookmarks.all.to_json
+# end
+#
+# post "/users/my_bookmarks" do
+#   Bookmark.create!(
+#   user_id: params[:user_id],
+#   bookmark_url: params[:bookmark_url],
+#   bookmark_name: params[:bookmark_name],
+#   bookmark_description: params[:bookmark_description]
+#   )
+# end
 
-post "/users/my_bookmarks" do
-  Bookmark.create!(
-  user_id: params[:user_id],
-  bookmark_url: params[:bookmark_url],
-  bookmark_name: params[:bookmark_name],
-  bookmark_description: params[:bookmark_description]
-  )
+get "/users/dummy" do
+  user = {
+    "1" => {"bookmark_name" => "Article", "bookmark_description" => "Great Read!", "bookmark_url" => "https://www.notawebsite.com/article"},
+    "2" => {"bookmark_name" => "Video", "bookmark_description" => "Cool cat video", "bookmark_url" => "https://www.notawebsite.com/catvideo"}
+  }
+  body user.to_json
 end
 end
