7c7c576d0a1100876a50b295a0e813cb0c75e638
Alyssa Pratt <snubly1@gmail.com>
2016-06-10 16:22:48 -0400

    push

--- Plock.rb
+++ Plock.rb
@@ -91,17 +91,19 @@ class Plock < Sinatra::Base
     # u = user params[:username], params[:password]
     #
     # nr = Recommendation.new(user_id: u.id, recipient_id: recipient.id, bookmark_id: bookmark.id)
+    sender = u.username
+    reciever = recipient.username
 
-
-    channel: "#plock_recommendations",
-    username: "Plock!",
-    text: "#{sender} recommended a link to #{reciever}! View it <https://www.theironyard.com|here!> ",
-    icon_emoji: ":aardworf:"
-  }
-  HTTParty.post "https://hooks.slack.com/services/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",
-    body: {
-      payload: data.to_json
+    data = {
+      channel: "#plock_recommendations",
+      username: "Plock!",
+      text: "#{sender} recommended a link to #{reciever}! View it <https://www.theironyard.com|here!> ",
+      icon_emoji: ":aardwolf:"
     }
+    HTTParty.post "https://hooks.slack.com/services/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",
+      body: {
+        payload: data.to_json
+      }
   end
 
   # get "/recommendations" do
