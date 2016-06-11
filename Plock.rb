482d5a01b04ca3e85e0688f370b445ada60a6343
Alyssa Pratt <snubly1@gmail.com>
2016-06-11 10:39:35 -0400

    final working slack post

--- Plock.rb
+++ Plock.rb
@@ -94,10 +94,11 @@ class Plock < Sinatra::Base
     sender = u.username
     reciever = recipient.username
 
+    #THIS WORKS WOOT WOOT!
     data = {
       channel: "#plock_recommendations",
       username: "Plock!",
-      text: "#{sender} recommended a link to #{reciever}! View it <https://www.theironyard.com|here!> ",
+      text: "@#{sender} recommended a link to @#{reciever}! View it <https://www.theironyard.com|here!> ",
       icon_emoji: ":aardwolf:"
     }
     HTTParty.post "https://hooks.slack.com/services/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",
